c7298069b63d5e9de672969c2e06de3b44f31c82
ianrich69420 <ian.orieko@student.moringaschool.com>
2022-12-15 02:08:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  function reversedWord(word) {
+    return word.toString().split("").reverse().join("");
+  }
+
+  function isPalindrome(word) {
+    const reversedWord = reverseWord(word)
+    if (word === reversedWord) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
 /* 
   Add your pseudocode here
+  
+  function that takes in a word
+    compares that word with the word written backwards
+    if the word is the same backwards
+      return true
+    else
+      return false
+
 */
 
 /*
   Add written explanation of your solution here
+  Two functions are used to check if the word is a palindrome
+  One function converts the word into a string in which each letter is split into an array, that array is reversed then converted back to a string
+  The second function takes the inputted word, compares it with the same word reversed by the other function and outputs true or false depending on whether the word is a palindrome or not
 */
 
 // You can run `node index.js` to view these console logs
